74b47c5d85e2c26f0d663a2b7d35328535571702
sarthaksavvy <sarthak.shrivastava@pfizer.com>
2021-09-15 20:07:20 +0530

    with db directory

---
+++ src/Database/Factories/SocialProfileFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Bitfumes\Breezer\Database\Factories;
+
+use Bitfumes\Breezer\SocialProfile;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Bitfumes\Breezer\Tests\Database\Factories\UserFactory;
+
+class SocialProfileFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model
+     *
+     * @var string
+     */
+    protected $model = SocialProfile::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'id'               => $this->faker->randomNumber,
+            'user_id'          => function () {
+                return UserFactory::new()->create()->id;
+            },
+            'service_id' => 'asdfasdfadsfadsf',
+            'avatar'     => $this->faker->url,
+            'service'    => 'google',
+        ];
+    }
+}

8d1c8ae1742fccfe1de7f9db41b4152908ecd73b
sarthaksavvy <sarthak@bitfumes.com>
2022-01-14 11:01:58 +0530

    removed auto load package migration

--- src/BreezerServiceProvider.php
+++ src/BreezerServiceProvider.php
@@ -7,56 +7,55 @@ use Illuminate\Support\ServiceProvider;
 
 class BreezerServiceProvider extends ServiceProvider
 {
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        $this->mergeConfigFrom(__DIR__ . '/../config/breezer.php', 'breezer');
-        $this->publishThings();
-        $this->loadViewsFrom(__DIR__ . '/Views', 'breezer');
-        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
-        $this->registerRoutes();
-    }
+	/**
+	 * Bootstrap any application services.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->mergeConfigFrom(__DIR__ . '/../config/breezer.php', 'breezer');
+		$this->publishThings();
+		$this->loadViewsFrom(__DIR__ . '/Views', 'breezer');
+		$this->registerRoutes();
+	}
 
-    /**
-     * Register the package routes.
-     *
-     * @return void
-     */
-    private function registerRoutes()
-    {
-        Route::group($this->routeConfiguration(), function () {
-            $this->loadRoutesFrom(__DIR__ . '/Http/routes.php');
-        });
-    }
+	/**
+	 * Register the package routes.
+	 *
+	 * @return void
+	 */
+	private function registerRoutes()
+	{
+		Route::group($this->routeConfiguration(), function () {
+			$this->loadRoutesFrom(__DIR__ . '/Http/routes.php');
+		});
+	}
 
-    /**
-    * Get the Blogg route group configuration array.
-    *
-    * @return array
-    */
-    private function routeConfiguration()
-    {
-        return [
-            'namespace'  => "Bitfumes\Breezer\Http\Controllers",
-            'middleware' => 'api',
-            'prefix'     => 'api',
-        ];
-    }
+	/**
+	* Get the Blogg route group configuration array.
+	*
+	* @return array
+	*/
+	private function routeConfiguration()
+	{
+		return [
+			'namespace'  => "Bitfumes\Breezer\Http\Controllers",
+			'middleware' => 'api',
+			'prefix'     => 'api',
+		];
+	}
 
-    public function publishThings()
-    {
-        if ($this->app->runningInConsole()) {
-            $this->publishes([
-                __DIR__ . '/../config/Breezer.php' => config_path('breezer.php'),
-            ], 'config');
+	public function publishThings()
+	{
+		if ($this->app->runningInConsole()) {
+			$this->publishes([
+				__DIR__ . '/../config/Breezer.php' => config_path('breezer.php'),
+			], 'config');
 
-            $this->publishes([
-                __DIR__ . '/database/migrations/' => database_path('migrations'),
-            ], 'breezer:migrations');
-        }
-    }
+			$this->publishes([
+				__DIR__ . '/database/migrations/' => database_path('migrations'),
+			], 'breezer:migrations');
+		}
+	}
 }

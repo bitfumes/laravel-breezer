0abd5d7490e036d9cfd81deccf85a72daf8b4826
sarthaksavvy <sarthak@bitfumes.com>
2022-01-23 13:43:22 +0530

    handle wrong old password

--- src/Http/Controllers/UpdatePasswordController.php
+++ src/Http/Controllers/UpdatePasswordController.php
@@ -3,6 +3,7 @@
 namespace Bitfumes\Breezer\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Symfony\Component\HttpFoundation\Response;
 
 class UpdatePasswordController extends AuthController
@@ -21,6 +22,16 @@ class UpdatePasswordController extends AuthController
 			'oldPassword' => 'required',
 			'password'    => 'required|confirmed',
 		]);
+
+		$user = auth()->user();
+
+		if (! $user || ! Hash::check($request->oldPassword, $user->password)) {
+			return response()->json([
+				'errors' => [
+					'error' => 'Credentials does\'t match our record',
+				],
+			], 401);
+		}
 
 		auth()->user()->update(['password' => bcrypt($data['password'])]);
 		return response('password successfully changed', Response::HTTP_ACCEPTED);
--- tests/Feature/UpdatePasswordTest.php
+++ tests/Feature/UpdatePasswordTest.php
@@ -13,14 +13,28 @@ class UpdatePasswordTest extends TestCase
 	/**
 	 * @test
 	 */
-	public function a_password_reset_link_email_can_be_sent()
+	public function a_password_can_be_changed_by_user()
 	{
 		$user = $this->authUser();
 		$this->postJson(route('user.password.update'), [
-			'oldPassword'           => 'secret',
+			'oldPassword'           => 'secret123',
 			'password'              => '123456',
 			'password_confirmation' => '123456',
 		])->assertStatus(202);
 		$this->assertTrue(Hash::check('123456', $user->fresh()->password));
+	}
+
+	/**
+	 * @test
+	 */
+	public function a_password_can_be_not_be_changed_by_user_if_old_password()
+	{
+		$user = $this->authUser();
+		$this->postJson(route('user.password.update'), [
+			'oldPassword'           => 'wrongPassword',
+			'password'              => '123456',
+			'password_confirmation' => '123456',
+		])->assertStatus(401);
+		$this->assertFalse(Hash::check('123456', $user->fresh()->password));
 	}
 }
